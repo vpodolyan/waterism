44af4e5ecc1b3f68ec3e2939f51d2f883021f04d
Vladimir Podolyan <vpodolyan@outlook.com>
2023-01-31 17:31:52 +0300

    Add stroing data in mmkv

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
+  StyleSheet,
   Text,
   useColorScheme,
+  View,
 } from 'react-native';
+import { useMMKVNumber } from 'react-native-mmkv';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -14,8 +18,11 @@ function App() {
     backgroundColor: isDarkMode ? '#000' : '#fff',
   };
 
+  const [totalBottlesDrunk, setTotalBottlesDrunk] =
+    useMMKVNumber('totalBottlesDrunk');
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={[styles.safeArea, backgroundStyle]}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
@@ -23,11 +30,45 @@ function App() {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}
+        contentContainerStyle={styles.contentContainer}
       >
-        <Text>Initial Commit</Text>
+        <View style={styles.titleView}>
+          <Text style={styles.bottlesCount}>
+            Total bottles: {totalBottlesDrunk}
+          </Text>
+        </View>
+        <View style={styles.addButtonView}>
+          <Button
+            title="Add bottle"
+            onPress={() => {
+              setTotalBottlesDrunk((current) => (current || 0) + 1);
+            }}
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  bottlesCount: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    flexGrow: 1,
+    padding: 12,
+  },
+  titleView: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  addButtonView: {
+    alignItems: 'flex-end',
+  },
+});
 
 export default App;

124787e171ec6e0f9862ac2b0aef267ddbbd6c9d
Vladimir Podolyan <vpodolyan@outlook.com>
2023-06-09 00:27:25 +0300

    Simple control to select months on Stasts screen

--- src/screens/StatsScreen/StatsScreen.tsx
+++ src/screens/StatsScreen/StatsScreen.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { VictoryBar, VictoryChart, VictoryTheme } from 'victory-native';
 import { useYearStats } from '../../store/utils/useYearStats';
 
 export function StatsScreen() {
   const [yearStats] = useYearStats(new Date());
-  const monthIndex = new Date().getMonth();
+  const [monthIndex, setMonthIndex] = useState(new Date().getMonth());
+
+  const selectNextMonth = () => {
+    setMonthIndex((value) => (value < 11 ? value + 1 : value));
+  };
+
+  const selectPrevMonth = () => {
+    setMonthIndex((value) => (value > 0 ? value - 1 : value));
+  };
+
   const chartData = yearStats?.[monthIndex]
     ? Object.keys(yearStats[monthIndex]).map((day) => ({
         date: day,
@@ -13,8 +22,20 @@ export function StatsScreen() {
       }))
     : [];
 
+  const monthDate = new Date();
+  monthDate.setMonth(monthIndex);
+
   return (
     <View style={styles.container}>
+      <View style={styles.row}>
+        <Pressable onPress={selectPrevMonth}>
+          <Text style={styles.text}>{'< '}</Text>
+        </Pressable>
+        <Text style={styles.text}>{monthDate.toLocaleDateString('en', { month: 'long' })}</Text>
+        <Pressable onPress={selectNextMonth}>
+          <Text style={styles.text}>{' >'}</Text>
+        </Pressable>
+      </View>
       <VictoryChart width={400} theme={VictoryTheme.material}>
         {!!yearStats && yearStats[monthIndex] && (
           <VictoryBar
@@ -37,5 +58,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#f5fcff'
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  text: {
+    fontSize: 20
   }
 });
